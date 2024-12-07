82bc5a13db576867878d8fb641039c9e34b1fb63
Kicks <nolandimensions@gmail.com>
2024-12-06 21:58:57 -0500

    Update HomeSection.svelte

--- src/lib/HomePage/HomeSection.svelte
+++ src/lib/HomePage/HomeSection.svelte
@@ -1,5 +1,5 @@
 <div class="flex all-center w-full h-screen text-white p-5 relative">
-    <div class="flex flex-col items-start justify-evenly p-5 bg-primary rounded-lg">
+    <div class="flex flex-col items-start justify-evenly p-5 bg-primary rounded-lg sm:-translate-y-0 -translate-y-7">
         <img src="/largelogoalt.png" class="h-32 rounded-md object-contain" alt="Logo" />
         <div class="flex gap-1 w-full animate-bounce sm:text-2xl text-md all-center">
             <p>Making</p>
