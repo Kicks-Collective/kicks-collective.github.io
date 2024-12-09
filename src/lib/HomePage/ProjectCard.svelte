79d90984a9e8e43be610ec474c559920ff8b924e
Kicks <nolandimensions@gmail.com>
2024-12-08 20:37:34 -0500

    Fixes

--- src/lib/HomePage/ProjectCard.svelte
+++ src/lib/HomePage/ProjectCard.svelte
@@ -2,26 +2,26 @@
     let { project }: { project: GameProject } = $props();
 </script>
 
-<div class="flex flex-row items-start bg-primary rounded-md p-3 h-52 gap-2 w-full justify-evenly">
+<div class="flex flex-row items-center bg-primary rounded-md p-3 h-52 gap-2 w-full justify-evenly">
     
     <div class="h-full flex w-full flex-col justify-evenly">
 
         {#if project.link}
-            <a href={project.link} class="text-2xl hover:text-alt transition-all">{project.title}</a>
+            <a href={project.link} class="text-xl hover:text-alt transition-all">{project.title}</a>
         {:else}
-            <p class="text-2xl transition-all">{project.title}</p>
+            <p class="text-xl transition-all">{project.title}</p>
         {/if}
         
         {#if project.description}
-            <p class="text-base">{project.description}</p>
+            <p class="text-sm md:text-lg">{project.description}</p>
         {/if}
 
         {#if project.status}
-            <p class="text-base">{project.status}</p>
+            <p class="text-sm md:text-base">{project.status}</p>
         {/if}
     </div>
 
     {#if project.image}
-        <img src={project.image} alt={project.title} class="h-full aspect-square rounded-md flex" />
+        <img src={project.image} alt={project.title} class="sm:h-full h-32 aspect-square rounded-md flex" />
     {/if}
 </div>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
 <div class="flex all-center w-full h-screen text-white flex-col">
     <img src="/forts.png" class="absolute -z-10 w-full h-full object-cover blur" alt="Fortwars" />
     <div class="text-2xl">Page not found</div>
-    <a href="/" class="hover:text-blue-200 transition-all grayscale-[35%]">Go back home</a>
+    <a href="/" class="hover:text-alt transition-all grayscale-[35%]">Go back home</a>
 </div>
